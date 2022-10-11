9b9db06e73df6ace5aeb6c77532f05c188c599da
Mika Nokka <mika.nokka@digital14.com>
2022-10-11 14:56:52 +0300

    Changing security setting based on user group existence checking works

---
+++ README.md
@@ -0,0 +1,2 @@
+Set Jira issue security based on user group, when default security setting is too restricting for this user/group   
+Used as a postfunction via ScriptRunner functionality (Data Center version)
--- SecuritySetter.groovy
+++ SecuritySetter.groovy
@@ -1,9 +1,11 @@
 /*
  October 2022 mika.nokka1@gmail.com
  
- Project has several security level settings, default is the most restricted onbe. When user not having rights for default settings creates ticket,
- visibility disappear. This postfunction sets newly created issue security according user group (if not in default rights group)
+ Project has several security level settings, default is the most restricted onbe. When user not having rights
+ for default settings creates ticket, visibility disappears. This postfunction sets newly created issue 
+ security according user group (if not in default rights group). Correct users are in two defined groups (only)
  
+ For Jira Data Center usage
  */
 
 import com.atlassian.jira.component.ComponentAccessor
@@ -27,15 +29,17 @@ def issueManager= ComponentAccessor.issueManager
 def issueService = ComponentAccessor.issueService
 
 def groupManager = ComponentAccessor.groupManager
-
+def securityLevelManager = ComponentAccessor.getComponent(IssueSecurityLevelManager)
 
 
 
 // CONFIGURATIONS
-def group1="xxx"" // first required  group for correct user indentification
-def group2="yyy"  // second required  group for correct user indentification
+def group1="xxx" // first required  group for correct user indentification
+def group2="yyy" // second required  group for correct user indentification
 def groupsCounter=0  // counter for allowed and required group partisipation
 def allowedGroupnsNmbr=2 // max number of groups for the user
+final String commenttext = "FYI:Issue security setting was changed from default by automation"
+final boolean dispatchEvent = true // send event after commenting
 //
 
 
@@ -48,12 +52,12 @@ log.debug("---------- IssueSecuritySetter started -----------")
 
 def util = ComponentAccessor.getUserUtil()
 whoisthis=ComponentAccessor.getJiraAuthenticationContext().getUser()
-log.debug("Script run as a user: {$whoisthis}")
+log.debug("Issue: {$issue} --> Script run as a user: {$whoisthis}")
 
 def groupNames = groupManager.getGroupNamesForUser(whoisthis)
-log.debug("User: {$groupNames}")
 def numberOfGroups=groupNames.size()
-log.debug("Number of groups: {$numberOfGroups}")
+
+log.debug("User's groups {$numberOfGroups}: {$groupNames}")
 
 
 // in POC case, user with limited access, only in two user groups
@@ -67,7 +71,7 @@ else {
 
 
 if  (ComponentAccessor.groupManager.isUserInGroup(whoisthis, group2)) {
-	log.debug("User IS in group: {$group1}")
+	log.debug("User IS in group: {$group2}")
 	groupsCounter=groupsCounter+1
 }
 else {
@@ -75,13 +79,53 @@ else {
 }
 
 
+// check if security settings is needed to be done
 if(allowedGroupnsNmbr==groupsCounter) {
-	log.debug("User is only allowed groups: {$allowedGroupnsNmbr}")
-	log.debug("Going to do issue security settings change")
+	log.debug("User is only allowed groups {$allowedGroupnsNmbr}:  $group1 , $group2")
+	log.debug("==> Going to do issue security settings change")
+	
+	def securityLevelId = issue.securityLevelId
+	def securityLevelName=securityLevelManager.getIssueSecurityName(securityLevelId)
+	log.debug("Current Issue Security: ${securityLevelName}")
+	
+	//use account having rights to do the security settings
+	
+	// capture context of original user
+	def authContext = ComponentAccessor.getJiraAuthenticationContext()
+	
+	//try and assert, maybe too much
+	try{
+		
+		 authContext.setLoggedInUser(ComponentAccessor.getUserManager().getUserByName("mika.nokka")); // use script account
+		 
+		 // force sets IssueSecurityLevel to this ID 
+		 def issueSecurityLevelId=10200 //system spesific level id from Jira UI
+		 def issueInputParameters = new IssueInputParametersImpl()
+		 issueInputParameters.setSecurityLevelId(issueSecurityLevelId)
+
+		 def updateValidationResult = issueService.validateUpdate(loggedInUser, issue.id, issueInputParameters)
+		 assert updateValidationResult.valid : updateValidationResult.errorCollection
+
+		 def issueUpdateResult = issueService.update(loggedInUser, updateValidationResult, EventDispatchOption.ISSUE_UPDATED, false)
+		 assert issueUpdateResult.valid : issueUpdateResult.errorCollection
+		 log.debug("Force set Issue($issue) Security id as: ${issueSecurityLevelId}")
+		
+		}catch(Exception e){
+		 e.printStackTrace();
+		}finally {
+		 log.error("ERROR: SCript failed to change user for issue security operation")
+		 authContext.setLoggedInUser(whoisthis);
+		}
+	
+
+	authContext.setLoggedInUser(whoisthis);
+	ComponentAccessor.commentManager.create(issue, whoisthis, commenttext, dispatchEvent)
+	
 }
-else {
-	log.debug("User is listed in more than allowed groups: {$numberOfGroups}.  (allowed:{$allowedGroupnsNmbr}")
-	log.debug("Not going to do any issue security settings")
+else if ( (numberOfGroups > allowedGroupnsNmbr) || (groupsCounter !=  allowedGroupnsNmbr))     {
+	log.debug("ERROR: User is listed in more than allowed groups: {$numberOfGroups}.  (allowed:{$allowedGroupnsNmbr}")
+	log.debug("ERROR: Or user is not member of all needed groups" )
+	log.debug("==> Not going to do any issue security settings")
 }
 
 
